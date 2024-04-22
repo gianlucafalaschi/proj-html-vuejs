fa87fd6e7e60c87f40da030c6d06b3c03351308b
gianlucafalaschi <gian100gian@outlook.it>
2024-04-22 12:50:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+  <h1>Ciao</h1>
+</template>
+
+<style lang="scss">
+@use './style/generic';
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
