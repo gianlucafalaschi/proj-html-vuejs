15ebbf288938b5f5a41ed3c60aa6f0d5798c374c
gianlucafalaschi <gian100gian@outlook.it>
2024-04-22 23:48:48 +0200

    cards and button

--- src/components/AppNewCourses.vue
+++ src/components/AppNewCourses.vue
@@ -4,17 +4,213 @@
 
 
 <template>
-    <h1>sono nuovi corsi</h1>
+    <section>
+        <div class="ms-container-80">
+            <div class="ms-row text-center">
+                <h2 class="py-4 mb-0">New Courses</h2>
+            </div>
+            <div class="ms-row d-flex flex-wrap pb-4">
+                <!-- card 1 -->
+                <div class="ms-card">
+                    <div class="ms-image-container">
+                        <img class="ms-card-image" src="../assets/img/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="">
+                    </div>
+                    <div class="ms-card-description p-3">
+                        <h4 class="ms-card-title text-center pb-3 mb-0">How to be a DJ? Make Electronic Music</h4>
+                        <div class="ms-card-subtitle text-center pb-3">Electronic</div>
+                        <div class="ms-card-info pt-2 d-flex  justify-content-between flex-wrap">
+                            <div>
+                                <span class="ms-icon me-1"><i class="fa-solid fa-signal"></i></span>
+                                <span>Advanced</span>
+                            </div>
+                            <div>
+                                <span class="ms-icon me-1" ><i class="fa-solid fa-list"></i></span>
+                                <span>8 Lectures</span>
+                            </div>
+                            <div>
+                                <span class="ms-icon me-1"><i class="fa-regular fa-clock"></i></span>
+                                <span>6 hours</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/ card 1 -->
+                <!-- card 2 -->
+                <div class="ms-card">
+                    <div class="ms-image-container">
+                        <img class="ms-card-image" src="../assets/img/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="">
+                    </div>
+                    <div class="ms-card-description p-3">
+                        <h4 class="ms-card-title text-center pb-3 mb-0">How to be a DJ? Make Electronic Music</h4>
+                        <div class="ms-card-subtitle text-center pb-3">Electronic</div>
+                        <div class="ms-card-info pt-2 d-flex  justify-content-between flex-wrap">
+                            <div>
+                                <span class="ms-icon me-1"><i class="fa-solid fa-signal"></i></span>
+                                <span>Advanced</span>
+                            </div>
+                            <div>
+                                <span class="ms-icon me-1" ><i class="fa-solid fa-list"></i></span>
+                                <span>8 Lectures</span>
+                            </div>
+                            <div>
+                                <span class="ms-icon me-1"><i class="fa-regular fa-clock"></i></span>
+                                <span>6 hours</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/ card 2 -->
+                <!-- card 3 -->
+                <div class="ms-card">
+                    <div class="ms-image-container">
+                        <img class="ms-card-image" src="../assets/img/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="">
+                    </div>
+                    <div class="ms-card-description p-3">
+                        <h4 class="ms-card-title text-center pb-3 mb-0">How to be a DJ? Make Electronic Music</h4>
+                        <div class="ms-card-subtitle text-center pb-3">Electronic</div>
+                        <div class="ms-card-info pt-2 d-flex  justify-content-between flex-wrap">
+                            <div>
+                                <span class="ms-icon me-1"><i class="fa-solid fa-signal"></i></span>
+                                <span>Advanced</span>
+                            </div>
+                            <div>
+                                <span class="ms-icon me-1" ><i class="fa-solid fa-list"></i></span>
+                                <span>6 Lectures</span>
+                            </div>
+                            <div>
+                                <span class="ms-icon me-1"><i class="fa-regular fa-clock"></i></span>
+                                <span>6 hours</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/ card 3 -->
+                <!-- card 4 -->
+                <div class="ms-card">
+                    <div class="ms-image-container">
+                        <img class="ms-card-image" src="../assets/img/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="">
+                    </div>
+                    <div class="ms-card-description p-3">
+                        <h4 class="ms-card-title text-center pb-3 mb-0">How to be a DJ? Make Electronic Music</h4>
+                        <div class="ms-card-subtitle text-center pb-3">Electronic</div>
+                        <div class="ms-card-info pt-2 d-flex  justify-content-between flex-wrap">
+                            <div>
+                                <span class="ms-icon me-1"><i class="fa-solid fa-signal"></i></span>
+                                <span>Intermediate</span>
+                            </div>
+                            <div>
+                                <span class="ms-icon me-1" ><i class="fa-solid fa-list"></i></span>
+                                <span>6 Lectures</span>
+                            </div>
+                            <div>
+                                <span class="ms-icon me-1"><i class="fa-regular fa-clock"></i></span>
+                                <span>6 hours</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/ card 4 -->
+                <!-- card 5 -->
+                <div class="ms-card">
+                    <div class="ms-image-container">
+                        <img class="ms-card-image" src="../assets/img/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="">
+                    </div>
+                    <div class="ms-card-description p-3">
+                        <h4 class="ms-card-title text-center pb-3 mb-0">How to be a DJ? Make Electronic Music</h4>
+                        <div class="ms-card-subtitle text-center pb-3">Electronic</div>
+                        <div class="ms-card-info pt-2 d-flex  justify-content-between flex-wrap">
+                            <div>
+                                <span class="ms-icon me-1"><i class="fa-solid fa-signal"></i></span>
+                                <span>Intermediate</span>
+                            </div>
+                            <div>
+                                <span class="ms-icon me-1" ><i class="fa-solid fa-list"></i></span>
+                                <span>6 Lectures</span>
+                            </div>
+                            <div>
+                                <span class="ms-icon me-1"><i class="fa-regular fa-clock"></i></span>
+                                <span>6 hours</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/ card 5 -->
+                <!-- card 6 -->
+                <div class="ms-card">
+                    <div class="ms-image-container">
+                        <img class="ms-card-image" src="../assets/img/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="">
+                    </div>
+                    <div class="ms-card-description p-3">
+                        <h4 class="ms-card-title text-center pb-3 mb-0">How to be a DJ? Make Electronic Music</h4>
+                        <div class="ms-card-subtitle text-center pb-3">Electronic</div>
+                        <div class="ms-card-info pt-2 d-flex  justify-content-between flex-wrap">
+                            <div>
+                                <span class="ms-icon me-1"><i class="fa-solid fa-signal"></i></span>
+                                <span>Intermediate</span>
+                            </div>
+                            <div>
+                                <span class="ms-icon me-1" ><i class="fa-solid fa-list"></i></span>
+                                <span>6 Lectures</span>
+                            </div>
+                            <div>
+                                <span class="ms-icon me-1"><i class="fa-regular fa-clock"></i></span>
+                                <span>6 hours</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/ card 6 -->
+            </div>
+            <div class="ms-row ms-row-buttons d-flex justify-content-center pb-5">
+                <button class="ms-button" type="button">LOAD MORE</button>
+            </div>
+        </div>
+
+    </section>
+
 </template>
 
 <style scoped lang="scss">
 @use'../style/partials/_variables' as *;
 
 section {
-    background-color:$color-fifth;
+    background-color: $color-fifth;
+}
+
+.ms-card {
+    width: calc((100% / 3) - 24px);
+    margin-left: 12px;
+    margin-right: 12px;
+    margin-top:12px;
+    margin-bottom:12px;
+    background-color: $color-first;
+}
+
+.ms-image-container {
+    width: 100%;
+
+    .ms-card-image {
+        width: 100%;
+    }
 }
 
 
+.ms-card-subtitle {
+    color: $color-sixth;
+    border-bottom: 1px solid $color-sixth ;
+}
 
+.ms-icon {
+    color: $color-second;
+}
 
+.ms-button {
+    color:$color-first;
+    background-color: $color-second;
+    border: none;
+    border-radius: 20px;
+    padding: 6px 10px;
+    font-weight: bold;
+    font-size: 14px;
+}
 </style>
