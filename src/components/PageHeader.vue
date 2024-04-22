3a1cb500729117f1220659065836110dc286a026
gianlucafalaschi <gian100gian@outlook.it>
2024-04-22 16:44:20 +0200

    header sections

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -4,14 +4,39 @@
 
 
 <template>
-    <h1>Sono l'header</h1>
+    <header>
+        <div class="ms-container-80">
+            <div class="ms-row d-flex justify-content-between align-items-center py-4">
+                <div class="ms-logo-section d-flex">Immagine master study</div>
+                <div class="ms-links-socials d-flex">
+                    <div class="ms-links-section me-4">Links</div>
+                    <div class="ms-socials-section d-flex">Socials</div>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
 
 <style scoped lang="scss">
 @use'../style/partials/_variables' as *;
 
+
+header {
+    background-color: $color-first;
+}
+
 h1 {
     color: $color-second;
+}
+
+.ms-container-80 {
+border: 1px dashed black;
+  
+}
+
+.ms-row {
+    border: 1px dashed black;
+
 }
 
 </style>
