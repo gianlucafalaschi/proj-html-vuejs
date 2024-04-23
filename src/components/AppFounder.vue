fe05845ee1c13413c21e988b0810cd298d05cbb9
gianlucafalaschi <gian100gian@outlook.it>
2024-04-23 11:35:59 +0200

    email input and button

--- src/components/AppFounder.vue
+++ src/components/AppFounder.vue
@@ -7,7 +7,15 @@
     <section class="founder">
         <div class="ms-container-85">
             <div class="ms-row ms-row-offers d-flex rounded-2 ">
-                <div class="ms-col-50 p-4">Ciao</div>
+                <div class="ms-col-50 p-4">
+                    <div class="ms-title-offer">
+                        <h4>Limited Sale: All courses with 55% off</h4>
+                    </div>
+                    <div class="ms-input">
+                        <input class="input-email" type="email" placeholder="Enter your email">
+                        <button class="ms-button" type="button">SUBCRIBE</button>
+                    </div>
+                </div>
                 <div class="ms-col-50 p-4">ciao</div>
             </div>
         </div>
@@ -15,12 +23,19 @@
             <div class="ms-row d-flex my-5">
                 <div class="ms-col-50 ms-col-50-left my-5 pe-5">
                     <h3>Hey there, my name is John Doe. I'm the founder of the Brain Academy</h3>
-                    <div class="signature-image-container mt-4 d-flex justify-content-center" >
+                    <div class="signature-image-container mt-4 d-flex justify-content-center">
                         <img src="../assets/img/sign.jpg" alt="founder's signature">
                     </div>
                 </div>
                 <div class="ms-col-50 ms-col-50-right my-5">
-                    <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary</p>
+                    <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring
+                        which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot,
+                        which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed
+                        in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be
+                        incapable of drawing a single stroke at the present moment; and yet I feel that I never was a
+                        greater artist than now. When, while the lovely valley teems with vapour around me, and the
+                        meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few
+                        stray gleams steal into the inner sanctuary</p>
                 </div>
             </div>
         </div>
@@ -31,7 +46,8 @@
 @use'../style/partials/_variables' as *;
 
 section {
-    background-color:$color-first;
+    background-color: $color-first;
+    position: relative;
 }
 
 .ms-container-85 {
@@ -40,10 +56,34 @@ section {
     margin: 0 auto;
     padding-left: 10px;
     padding-right: 10px;
-    border: 1px dashed  black;
+    border: 1px dashed black;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -70%);
+
 }
 
 .ms-row-offers {
     border: 1px dashed red;
+}
+
+.ms-input {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    position: relative;
+}
+
+.input-email {
+    border-radius: 20px;
+    width: 80%;
+    
+}
+
+.ms-button {
+    border-radius: 20px;
+    position: absolute;
+    left: 80%;
+    transform: translate(-50%);
 }
 </style>
